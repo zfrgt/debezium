509e7f55a8eebd7fea3c297e2190f57e7eafe36c
alexey.kotlyar <alexey.kotlyar@petabyte.technology>
2022-01-05 17:25:17 +0200

    Initial commit

---
+++ debezium-server/debezium-server-pubsub/README.md
@@ -0,0 +1,10 @@
+# PubSub change consumer for Debezium
+
+Instructions TBD.
+
+# Configuration
+
+```properties
+debezium.sink.pubsub.datacatalog.enabled=true
+debezium.sink.pubsub.pubsub.topic.prefix=pg-cdc-test
+```
--- debezium-server/debezium-server-pubsub/src/main/java/io/debezium/server/pubsub/PubSubChangeConsumer.java
+++ debezium-server/debezium-server-pubsub/src/main/java/io/debezium/server/pubsub/PubSubChangeConsumer.java
@@ -3,6 +3,9 @@
  *
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
+/**
+ * Adapted from https://github.com/GoogleCloudPlatform/DataflowTemplates/tree/main/v2/cdc-parent#deploying-the-connector
+ */
 package io.debezium.server.pubsub;
 
 import java.io.IOException;
@@ -32,19 +35,23 @@ import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
+import com.google.cloud.datacatalog.v1.Entry;
+import org.apache.beam.sdk.values.Row;
+import org.apache.kafka.connect.source.SourceRecord;
 
 import io.debezium.DebeziumException;
 import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.DebeziumEngine.RecordCommitter;
 import io.debezium.server.BaseChangeConsumer;
+import io.debezium.server.datacatalog.DataCatalogSchemaUtils;
+import io.debezium.server.datacatalog.DebeziumSourceRecordToDataflowCdcFormatTranslator;
 import io.debezium.server.CustomConsumerBuilder;
 
 /**
  * Implementation of the consumer that delivers the messages into Google Pub/Sub destination.
  *
  * @author Jiri Pechanec
- *
  */
 @Named("pubsub")
 @Dependent
@@ -62,10 +69,19 @@ public class PubSubChangeConsumer extends BaseChangeConsumer implements Debezium
     private String projectId;
 
     private final Map<String, Publisher> publishers = new HashMap<>();
+    private final DebeziumSourceRecordToDataflowCdcFormatTranslator translator = new DebeziumSourceRecordToDataflowCdcFormatTranslator();
+
+    private DataCatalogSchemaUtils.DataCatalogSchemaManager schemaUpdater;
     private PublisherBuilder publisherBuilder;
 
     @ConfigProperty(name = PROP_PREFIX + "ordering.enabled", defaultValue = "true")
     boolean orderingEnabled;
+
+    @ConfigProperty(name = PROP_PREFIX + "datacatalog.enabled", defaultValue = "true")
+    boolean datacatalogEnabled;
+
+    @ConfigProperty(name = PROP_PREFIX + "pubsub.topic.prefix", defaultValue = "pg-cdc-test")
+    String pubsubTopicPrefix;
 
     @ConfigProperty(name = PROP_PREFIX + "null.key", defaultValue = "default")
     String nullKey;
@@ -85,13 +101,19 @@ public class PubSubChangeConsumer extends BaseChangeConsumer implements Debezium
             return;
         }
 
+        if (datacatalogEnabled) {
+            // default mode is multi-topic, bc it's better to run multiple topic-aware DataCatalog schema updater with
+            // a single topic inside than vice versa.
+            schemaUpdater = DataCatalogSchemaUtils.getSchemaManager(projectId, pubsubTopicPrefix, false);
+            LOGGER.info("Initialized Data Catalog API usage, used prefix {} for PubSub topics", pubsubTopicPrefix);
+        }
+
         publisherBuilder = (t) -> {
             try {
                 return Publisher.newBuilder(t)
                         .setEnableMessageOrdering(orderingEnabled)
                         .build();
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 throw new DebeziumException(e);
             }
         };
@@ -104,9 +126,8 @@ public class PubSubChangeConsumer extends BaseChangeConsumer implements Debezium
         publishers.values().forEach(publisher -> {
             try {
                 publisher.shutdown();
-            }
-            catch (Exception e) {
-                LOGGER.warn("Exception while closing publisher: {}", e);
+            } catch (Exception e) {
+                LOGGER.warn("Exception while closing publisher: {}", e.getMessage());
             }
         });
     }
@@ -125,20 +146,33 @@ public class PubSubChangeConsumer extends BaseChangeConsumer implements Debezium
             if (orderingEnabled) {
                 if (record.key() == null) {
                     pubsubMessage.setOrderingKey(nullKey);
-                }
-                else if (record.key() instanceof String) {
+                } else if (record.key() instanceof String) {
                     pubsubMessage.setOrderingKey((String) record.key());
-                }
-                else if (record.key() instanceof byte[]) {
+                } else if (record.key() instanceof byte[]) {
                     pubsubMessage.setOrderingKeyBytes(ByteString.copyFrom((byte[]) record.key()));
                 }
             }
 
             if (record.value() instanceof String) {
                 pubsubMessage.setData(ByteString.copyFromUtf8((String) record.value()));
-            }
-            else if (record.value() instanceof byte[]) {
+            } else if (record.value() instanceof byte[]) {
                 pubsubMessage.setData(ByteString.copyFrom((byte[]) record.value()));
+            }
+
+            if (datacatalogEnabled) {
+                // only thing we apparently support for now
+                LOGGER.info("Trying to cast ChangeEvent to RecordChangeEvent");
+                LOGGER.info("ChangeEvent is not a RecordChangeEvent, not much we can do.");
+                Row updateRecord = translator.translate((SourceRecord) record);
+                if (updateRecord == null) {
+                    continue;
+                } else {
+                    Entry result = schemaUpdater.updateSchemaForTable(record.destination(), updateRecord.getSchema());
+                    if (result == null) {
+                        throw new InterruptedException(
+                                "A problem occurred when communicating with Cloud Data Catalog");
+                    }
+                }
             }
 
             deliveries.add(publisher.publish(pubsubMessage.build()));
@@ -147,8 +181,7 @@ public class PubSubChangeConsumer extends BaseChangeConsumer implements Debezium
         List<String> messageIds;
         try {
             messageIds = ApiFutures.allAsList(deliveries).get();
-        }
-        catch (ExecutionException e) {
+        } catch (ExecutionException e) {
             throw new DebeziumException(e);
         }
         LOGGER.trace("Sent messages with ids: {}", messageIds);
